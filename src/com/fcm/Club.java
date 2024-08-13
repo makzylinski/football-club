db5e971c9d155506c9a92d631fa3f9d1eed3ff3c
Maks <makzylinski@gmail.com>
2024-08-13 20:30:53 +0200

    delete player

--- src/com/fcm/Club.java
+++ src/com/fcm/Club.java
@@ -17,18 +17,40 @@ public class Club {
     }
 
     public void addPlayer(String playerName, String playerLastName, LocalDate dateOfBirth) {
-        if(checkPlayer(playerName, playerLastName)) {
+        if(checkPlayer(playerName, playerLastName) == null) {
             players.add(new Player(playerName, playerLastName, dateOfBirth));
+            System.out.println(playerName + " " + playerLastName + " has been added to the squad.");
         } else {
             System.out.println("Player is already part of the squad.");
         }
     }
 
-    private boolean checkPlayer(String playerName, String playerLastName) {
-        for (int i = 0; i < players.size() - 1; i++) {
-            return !players.get(i).getName().equalsIgnoreCase(playerName) ||
-                    !players.get(i).getLastName().equalsIgnoreCase(playerLastName);
+    public void deletePlayer(String playerName, String playerLastName) {
+        System.out.println(checkPlayer(playerName, playerLastName));
+        if(checkPlayer(playerName, playerLastName) != null) {
+            players.remove(checkPlayer(playerName, playerLastName));
+            System.out.println("Player has been deleted from the squad.");
+        } else {
+            System.out.println("Player is not part of the squad. Cannot delete.");
         }
-        return false;
     }
+
+    public void printSquad() {
+        players.forEach(player -> {
+            System.out.println(player.getName() + " " + player.getLastName());
+        });
+    }
+
+    private Player checkPlayer(String playerName, String playerLastName) {
+        for (int i = 0; i < players.size() - 1; i++) {
+            if(players.get(i).getName().equalsIgnoreCase(playerName)
+            && players.get(i).getLastName().equalsIgnoreCase(playerLastName)) {
+                // player exists in the squad
+                System.out.println(players.get(i));
+                return players.get(i);
+            }
+        }
+        return null;
+    }
+
 }
